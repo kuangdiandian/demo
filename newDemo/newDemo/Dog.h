8af3dfb6e93cc06e409770101b9d70a7b389e5b2
xiaomage <xiaomage@136.com>
2016-12-01 17:09:09 +0800

    t添加了一个类

---
+++ newDemo/newDemo/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  newDemo
+//
+//  Created by 邝贵斌 on 16/12/1.
+//  Copyright © 2016年 kuangguibin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ newDemo/newDemo/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  newDemo
+//
+//  Created by 邝贵斌 on 16/12/1.
+//  Copyright © 2016年 kuangguibin. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
